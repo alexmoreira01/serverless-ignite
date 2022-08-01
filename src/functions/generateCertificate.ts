d6cca25410ee89854ed0f41013679e89f7e580f6
Alex Moreira <alex.moreira.andrade@hotmail.com>
2022-07-31 21:18:08 -0300

    Aplicaççao completa

---
+++ src/functions/generateCertificate.ts
@@ -0,0 +1,117 @@
+import { APIGatewayProxyHandler } from "aws-lambda"
+import { document } from "../utils/dynamodbClient"
+import { compile } from "handlebars";
+import dayjs from "dayjs";
+import { join } from "path"
+import { readFileSync } from "fs";
+import { puppeteer }  from "chrome-aws-lambda";
+// import chromium, { puppeteer }  from "chrome-aws-lambda";
+import { S3 } from "aws-sdk"
+
+interface ICreateCertificate{
+  id: string;
+  name: string;
+  grade: string;
+}
+
+interface ITemplate{
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
+
+const compileTemplate = async (data: ITemplate) =>{
+  const filePath = join(process.cwd(), "src", "templates", "certificate.hbs")
+
+  const html = readFileSync(filePath, "utf-8");
+
+  return compile(html)(data)
+}
+
+export const handler: APIGatewayProxyHandler = async (event) =>{
+  const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate
+
+  const response = await document
+  .query({
+    TableName: "users_certificate",
+    KeyConditionExpression: "id = :id",
+    ExpressionAttributeValues:{
+      ':id': id
+    }
+  })
+  .promise();
+
+  const userAlreadyExists = response.Items[0];
+
+  if(!userAlreadyExists){
+    await document
+    .put({
+      TableName: "users_certificate",
+      Item: {
+        id,
+        name,
+        grade,
+        create_at: new Date().getTime(),
+      },
+    }).promise();
+
+  }
+
+ 
+    const medaPath = join(process.cwd(), "src", "templates", "selo.png");
+    const medal = readFileSync(medaPath, "base64");
+
+    const data: ITemplate = {
+      name, 
+      id, 
+      grade,
+      date: dayjs().format("DD/MM/YYYY"),
+      medal
+    }
+
+    const content = await compileTemplate(data)
+
+    const browser = await puppeteer.launch({
+      userDataDir: '/dev/null',
+    });
+
+    // const browser = await chromium.puppeteer.launch({
+    //   args: chromium.args,
+    //   defaultViewport: chromium.defaultViewport,
+    //   executablePath: await chromium.executablePath,
+    //   // headless: chromium.headless,
+    // });
+
+    const page = await browser.newPage();
+
+    await page.setContent(content)
+    const pdf = await page.pdf({
+      format: "a4",
+      landscape: true,
+      printBackground: true,
+      preferCSSPageSize: true,
+      path: process.env.IS_OFFLINE ? "./certificate.pdf" : null
+    })
+
+    await browser.close();
+
+    const s3 = new S3();
+
+    await s3.putObject({
+      Bucket:"certificate-nodejs-ignite2022",
+      Key: `${id}.pdf`,
+      ACL: "public-read",
+      Body: pdf,
+      ContentType: "application/pdf"
+    }).promise()
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      message: "Certificado criado com sucesso!",
+      url: `https://certificate-nodejs-ignite2022.s3.sa-east-1.amazonaws.com/${id}.pdf`
+    }) 
+  }
+}
---
+++ src/functions/verifyCertificate.ts
@@ -0,0 +1,43 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { document } from "../utils/dynamodbClient";
+
+interface IUserCertificate {
+  name: string;
+  id: string;
+  create_date: string;
+  grade: string;
+}
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const { id } = event.pathParameters;
+
+  const response = await document
+    .query({
+        TableName: "users_certificate",
+        KeyConditionExpression: "id = :id",
+        ExpressionAttributeValues:{
+        ':id': id
+        }
+    })
+    .promise();
+
+    const userCertificate = response.Items[0] as IUserCertificate;
+
+  if(userCertificate){
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        message: "Certificado válido",
+        name: userCertificate.name,
+        url: `https://certificate-nodejs-ignite2022.s3.sa-east-1.amazonaws.com/${id}.pdf`
+      })
+    };
+  }
+
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      message: "Certificado inválido",
+    }),
+  };
+}
